452c08dc2fc55d982a9fdbe8663eb1b59c16a6ad
huangminghua <huangminghua0615@163.com>
2023-02-22 16:27:49 +0800

    feat: 使用less

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const { series, parallel, src, dest, watch } = require('gulp')
 const htmlMin = require('gulp-htmlmin')
 const inject = require('gulp-inject')
 
+const less = require('gulp-less')
 const cssMin = require('gulp-cssmin')
 
 const babel = require('gulp-babel')
@@ -13,21 +14,18 @@ const htmlTask = () => {
   .pipe(htmlMin({
     collapseWhitespace: true // 压缩 html
   }))
-  .pipe(inject( 
-    src([ './src/js/**/*.js', './src/css/**/*.css' ], { read: false }), 
-    { relative: true } // 自动注入 js 和 css 
-  ))
   .pipe(dest('./dist/'))
 }
 
 const cssTask = () => {
-  return src('./src/css/**/*.css', { base: './src' })
+  return src('./src/css/**/*.less', { base: './src' }) // 保持对应目录结构不变
+  .pipe(less()) // 处理 less
   .pipe(cssMin()) // 压缩 css
   .pipe(dest('./dist/'))
 }
 
 const jsTask = () => {
-  return src('./src/js/**/*.js', { base: './src' })
+  return src('./src/js/**/*.js', { base: './src' }) 
   .pipe(babel({ presets: ['@babel/preset-env'] })) // 编译 ES6 -> ES5
   .pipe(uglify()) // 压缩 js
   .pipe(dest('./dist/'))
